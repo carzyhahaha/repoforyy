f9cf824dff552cb7312aa08c758f4b0c9dbc0c41
wushiyan <shiyan_w@gzleihou.cn>
2019-03-22 18:28:51 +0800

    ..

--- src/main/java/com/sy/coursechoosing/controller/AuthController.java
+++ src/main/java/com/sy/coursechoosing/controller/AuthController.java
@@ -4,16 +4,13 @@ package com.sy.coursechoosing.controller;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("auth")
 public class AuthController {
 
-    @GetMapping("login")
+    @PostMapping("login")
     public String login(String username, String password, Boolean rememberMe) {
 
         Subject subject = SecurityUtils.getSubject();
@@ -21,4 +18,15 @@ public class AuthController {
         subject.login(usernamePasswordToken);
         return "login";
     }
+
+    @GetMapping("logout")
+    public String logout() {
+        return "logout";
+    }
+
+    @GetMapping("error")
+    public String error() {
+        return "no auth";
+    }
+
 }
--- src/main/java/com/sy/coursechoosing/controller/test/TestController.java
+++ src/main/java/com/sy/coursechoosing/controller/test/TestController.java
@@ -4,6 +4,7 @@ package com.sy.coursechoosing.controller.test;
 import com.sy.coursechoosing.dao.TestDao;
 import com.sy.coursechoosing.entity.test.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -15,6 +16,9 @@ public class TestController {
 
     @Autowired
     TestDao testDao;
+
+    @Autowired
+    RedisTemplate<String, String> redisTemplate;
 
     @GetMapping("method1")
     public String method1() {
@@ -33,4 +37,18 @@ public class TestController {
         testDao.save(t);
         return "testDB successful";
     }
+
+    @GetMapping("testRedis")
+    public String testRedis(String key, String value) {
+
+        try {
+            redisTemplate.opsForValue().set(key, value);
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+
+        return "testRedis successful";
+    }
+
+
 }
